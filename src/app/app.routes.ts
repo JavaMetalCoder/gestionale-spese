a11a48df3b9486e70a76f9f91b631a3da2ab0eef
JavaMetalCoder <MetalCoder.dev@proton.me>
2026-05-06 18:26:26 +0200

    feat: implement Bootstrap styling and layout for the expense manager application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home-page/home-page';
+import { AggiungiSpesa } from './pages/aggiungi-spesa/aggiungi-spesa';
+import { RiepilogoSpesa } from './pages/riepilogo-spesa/riepilogo-spesa';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    title: 'Home | Gestionale Spese'
+  },
+  {
+    path: 'aggiungi-spesa',
+    component: AggiungiSpesa,
+    title: 'Aggiungi Spesa | Gestionale Spese'
+  },
+  {
+    path: 'riepilogo',
+    component: RiepilogoSpesa,
+    title: 'Riepilogo | Gestionale Spese'
+  }
+];
--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {}
+export class HomePage {
+  protected title = signal('Gestore Di Spese');
+}
